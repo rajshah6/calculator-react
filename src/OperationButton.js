2cdf75bb650db2e36f0b4a0acecdffb8a5938cb7
Raj Shah <rajnshah2020@gmail.com>
2025-01-15 21:37:13 -0500

    make number buttons and operations appear on display

---
+++ src/OperationButton.js
@@ -0,0 +1,13 @@
+import { actions } from "./App";
+
+export default function OperationButton({ dispatch, operation }) {
+  return (
+    <button
+      onClick={() =>
+        dispatch({ type: actions.CHOOSE_OPERATION, payload: { operation } })
+      }
+    >
+      {operation}
+    </button>
+  );
+}
